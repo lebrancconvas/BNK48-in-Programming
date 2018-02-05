b705a4861eed474565b9c827fc72ee8f25b37722
Lebranc Convas <lebrancconvas@gmail.com>
2018-02-06 04:33:42 +0700

    Edit Breakline Code Position.

--- idol-Shooting.py
+++ idol-Shooting.py
@@ -7,20 +7,23 @@ class idol():
         if(idshoot1 > idshoot2):
             print("{} {} {}".format(self.idol1,"SHOOT",self.idol2))
             print("{} {}".format(self.idol2,"DEFEATED"))
+            print("")
         elif(idshoot2 > idshoot1):
             print("{} {} {}".format(self.idol2,"SHOOT",self.idol1))
             print("{} {}".format(self.idol1,"DEFEATED"))
+            print("")
         else:
             print("{}".format("DRAW"))
+            print("")
 
 a = idol("Cherprang","Kaimook")
 a.shooting(random.randint(1, 10),random.randint(1, 10))        
-print("")
+
 b = idol("Miori","Jane")
 b.shooting(random.randint(1, 10),random.randint(1, 10)) 
-print("")  
+
 c = idol("Satchan","Pun")
 c.shooting(random.randint(1, 10),random.randint(1, 10))  
-print("") 
+
 d = idol("Music","Jennis")
 d.shooting(random.randint(1, 10),random.randint(1, 10))   
