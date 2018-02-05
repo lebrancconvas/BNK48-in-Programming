9625b045632abe230f074cfecf1b73e81d4c93c6
Lebranc Convas <lebrancconvas@gmail.com>
2018-02-05 16:43:19 +0700

    BNK48 First Gen Member List

---
+++ bnk48member.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+using namespace std;
+
+struct bnk48
+{
+  string nameMem;
+  int ageMem;
+};
+
+int main(){
+  bnk48 mem1;
+  mem1.nameMem = "Cherprang";
+  mem1.ageMem = 21;
+  
+  bnk48 mem2;
+  mem2.nameMem = "Can";
+  mem2.ageMem = 20;
+  
+  bnk48 mem3{"Ja",15};
+  bnk48 mem4{"Jan",23};
+  bnk48 mem5{"Jane",17};
+  bnk48 mem6{"Jennis",17};
+  bnk48 mem7{"Jib",15};
+  bnk48 mem8{"kaew",23};
+  bnk48 mem9{"Kaimook",20};
+  bnk48 mem10{"Kate",16};
+  bnk48 mem11{"Korn",19};
+  bnk48 mem12{"Mind",16};
+  bnk48 mem13{"Miori",19};
+  bnk48 mem14{"Mobile",15};
+  bnk48 mem15{"Music",16};
+  bnk48 mem16{"Namnueng",21};
+  bnk48 mem17{"Namsai",18};
+  bnk48 mem18{"Noey",20};
+  bnk48 mem19{"Orn",20};
+  bnk48 mem20{"Pun",17};
+  bnk48 mem21{"Pupae",20};
+  bnk48 mem22{"Satchan",14};
+  bnk48 mem23{"Tarwaan",21};
+  bnk48 mem24{"Isurina",22};
+  bnk48 mem25{"Mesa",18};
+  bnk48 mem26{"Namhom",15};
+  bnk48 mem27{"Nink",17};
+  bnk48 mem28{"Piam",14};
+  
+  cout << "BNK48 First Gen Member" << endl;
+  cout << "1. " << mem1.nameMem << "(" << mem1.ageMem << ")\n";
+  cout << "2. " << mem2.nameMem << "(" << mem2.ageMem << ")\n";
+  cout << "3. " << mem3.nameMem << "(" << mem3.ageMem << ")\n";
+  cout << "4. " << mem4.nameMem << "(" << mem4.ageMem << ")\n";
+  cout << "5. " << mem5.nameMem << "(" << mem5.ageMem << ")\n";
+  cout << "6. " << mem6.nameMem << "(" << mem6.ageMem << ")\n";
+  cout << "7. " << mem7.nameMem << "(" << mem7.ageMem << ")\n";
+  cout << "8. " << mem8.nameMem << "(" << mem8.ageMem << ")\n";
+  cout << "9. " << mem9.nameMem << "(" << mem9.ageMem << ")\n";
+  cout << "10. " << mem10.nameMem << "(" << mem10.ageMem << ")\n";
+  cout << "11. " << mem11.nameMem << "(" << mem11.ageMem << ")\n";
+  cout << "12. " << mem12.nameMem << "(" << mem12.ageMem << ")\n";
+  cout << "13. " << mem13.nameMem << "(" << mem13.ageMem << ")\n";
+  cout << "14. " << mem14.nameMem << "(" << mem14.ageMem << ")\n";
+  cout << "15. " << mem15.nameMem << "(" << mem15.ageMem << ")\n";
+  cout << "16. " << mem16.nameMem << "(" << mem16.ageMem << ")\n";
+  cout << "17. " << mem17.nameMem << "(" << mem17.ageMem << ")\n";
+  cout << "18. " << mem18.nameMem << "(" << mem18.ageMem << ")\n";
+  cout << "19. " << mem19.nameMem << "(" << mem19.ageMem << ")\n";
+  cout << "20. " << mem20.nameMem << "(" << mem20.ageMem << ")\n";
+  cout << "21. " << mem21.nameMem << "(" << mem21.ageMem << ")\n";
+  cout << "22. " << mem22.nameMem << "(" << mem22.ageMem << ")\n";
+  cout << "23. " << mem23.nameMem << "(" << mem23.ageMem << ")\n";
+  cout << "24. " << mem24.nameMem << "(" << mem24.ageMem << ")\n";
+  cout << "25. " << mem25.nameMem << "(" << mem25.ageMem << ")\n";
+  cout << "26. " << mem26.nameMem << "(" << mem26.ageMem << ")\n";
+  cout << "27. " << mem27.nameMem << "(" << mem27.ageMem << ")\n";
+  cout << "28. " << mem28.nameMem << "(" << mem28.ageMem << ")\n";
+  
+}
+
+/*
+Result:
+BNK48 First Gen Member
+1. Cherprang(21)
+2. Can(20)
+3. Ja(15)
+4. Jan(23)
+5. Jane(17)
+6. Jennis(17)
+7. Jib(15)
+8. kaew(23)
+9. Kaimook(20)
+10. Kate(16)
+11. Korn(19)
+12. Mind(16)
+13. Miori(19)
+14. Mobile(15)
+15. Music(16)
+16. Namnueng(21)
+17. Namsai(18)
+18. Noey(20)
+19. Orn(20)
+20. Pun(17)
+21. Pupae(20)
+22. Satchan(14)
+23. Tarwaan(21)
+24. Isurina(22)
+25. Mesa(18)
+26. Namhom(15)
+27. Nink(17)
+28. Piam(14)
+*/
