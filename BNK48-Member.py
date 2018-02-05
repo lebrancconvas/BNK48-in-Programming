7d5a9ceac7cf46328ea3a6c092ff5179e7a8cb74
Lebranc Convas <lebrancconvas@gmail.com>
2018-02-05 16:29:50 +0700

    BNK48 First Gen Member List

---
+++ BNK48-Member.py
@@ -0,0 +1,265 @@
+print("--BNK48 First Generation Member list--")
+
+class bnk48():
+  def __init__(self,name,fullname,age,birthplace,status):
+    self.name = name
+    self.fullname = fullname
+    self.age = age
+    self.birthplace = birthplace
+    self.status = status
+  def show(self):
+    print("> ",self.name)
+    print("FullName : ",self.fullname)
+    print("Age : ",self.age)
+    print("Birthplace : ",self.birthplace)
+    print("Status : ",self.status)
+    print("")
+    
+m1 = bnk48(name="Can",fullname="Nayika Srinian",age=20,birthplace="Bangkok",status="Team BIII")
+m2 = bnk48(name="Cherprang",fullname="Cherprang Areekul",age=21,birthplace="Bangkok",status="Team BIII")
+m3 = bnk48(name="Izurina",fullname="Rina Izuta",age=22,birthplace="	Saitama (Japan)",status="Team BIII")
+m4 = bnk48(name="Jaa",fullname="Napaphat Worraphuttanon",age=15,birthplace="Bangkok",status="Team BIII")
+m5 = bnk48(name="Jane",fullname="Kunjiranut Intarasin",age=17,birthplace="Nonthaburi",status="Team BIII")
+m6 = bnk48(name="Jennis",fullname="Jennis Oprasert",age=17,birthplace="Phetchaburi",status="Team BIII")
+m7 = bnk48(name="Jib",fullname="Suchaya Saenkhot",age=15,birthplace="Lop Buri",status="Team BIII")
+m8 = bnk48(name="Kaew",fullname="Natruja Chutiwansopon",age=23,birthplace="Chon Buri",status="Team BIII")
+m9 = bnk48(name="Kaimook",fullname="Warattaya Deesomlert",age=20,birthplace="Bangkok",status="Team BIII")
+m10 = bnk48(name="Kate",fullname="Korapat Nilprapa",age=16,birthplace="Phayao",status="Team BIII")
+m11 = bnk48(name="Korn",fullname="Vathusiri Phuwapunyasiri",age=19,birthplace="Bangkok",status="Team BIII")
+m12 = bnk48(name="Mind",fullname="Panisa Srilaloeng",age=16,birthplace="Nakhon Ratchasima",status="Team BIII")
+m13 = bnk48(name="Miori",fullname="Miori Ohkubo",age=19,birthplace="Ibaraki (Japan)",status="Team BIII")
+m14 = bnk48(name="Mobile",fullname="Pimrapat Phadungwatanachok",age=15,birthplace="Bangkok",status="Team BIII")
+m15 = bnk48(name="Music",fullname="Praewa Suthamphong",age=16,birthplace="Bangkok",status="Team BIII")
+m16 = bnk48(name="Namnueng",fullname="Milin Dokthian",age=21,birthplace="Sing Buri",status="Team BIII")
+m17 = bnk48(name="Namsai",fullname="Pichayapa Natha",age=18,birthplace="Bangkok",status="Team BIII")
+m18 = bnk48(name="Noey",fullname="Kanteera Wadcharathadsanakul",age=20,birthplace="Samut Prakan",status="Team BIII")
+m19 = bnk48(name="Orn",fullname="Patchanan Jiajirachote",age=21,birthplace="Bangkok",status="Team BIII")
+m20 = bnk48(name="Pun",fullname="Punsikorn Tiyakorn",age=17,birthplace="Bangkok",status="Team BIII")
+m21 = bnk48(name="Pupae",fullname="Jiradapa Intajak",age=20,birthplace="Chiang Rai",status="Team BIII")
+m22 = bnk48(name="Satchan",fullname="Sachiya Hanami / Sawitchaya Kajonrungsilp",age=14,birthplace="Bangkok",status="Team BIII")
+m23 = bnk48(name="Tarwann",fullname="Isarapa Thawatpakdee",age=21,birthplace="Nakhon Pathom",status="Team BIII")
+m24 = bnk48(name="Maysa",fullname="Mesa Chinavicharana",age=18,birthplace="Bangkok",status="Kenkyūsei")
+m25 = bnk48(name="Namhom",fullname="Christin Larsen",age=15,birthplace="Sisaket",status="Kenkyūsei")
+m26 = bnk48(name="Nink",fullname="Mananya Kaoju",age=18,birthplace="Samut Sakhon",status="Kenkyūsei")
+m27 = bnk48(name="Piam",fullname="Nayika Srinian",age=14,birthplace="Bangkok",status="Kenkyūsei")
+m28 = bnk48(name="Cincin",fullname="Rinrada Inthaisong",age=17,birthplace="Bangkok",status="Graduation")
+m29 = bnk48(name="Kitkat",fullname="Watsamon Phongwanit",age=16,birthplace="Bangkok",status="Graduation")
+m30 = bnk48(name="Jan",fullname="Jetsupa Kruetang",age=23,birthplace="Singburi",status="Graduation")
+
+
+m1.show()
+m2.show()
+m3.show()
+m4.show()
+m5.show()
+m6.show()
+m7.show()
+m8.show()
+m9.show()
+m10.show()
+m11.show()
+m12.show()
+m13.show()
+m14.show()
+m15.show()
+m16.show()
+m17.show()
+m18.show()
+m19.show()
+m20.show()
+m21.show()
+m22.show()
+m23.show()
+m24.show()
+m25.show()
+m26.show()
+m27.show()
+m28.show()
+m29.show()
+m30.show()    
+
+"""
+Result:
+
+--BNK48 First Generation Member list--
+>  Can
+FullName :  Nayika Srinian
+Age :  20
+Birthplace :  Bangkok
+Status :  Team BIII
+
+>  Cherprang
+FullName :  Cherprang Areekul
+Age :  21
+Birthplace :  Bangkok
+Status :  Team BIII
+
+>  Izurina
+FullName :  Rina Izuta
+Age :  22
+Birthplace :  	Saitama (Japan)
+Status :  Team BIII
+
+>  Jaa
+FullName :  Napaphat Worraphuttanon
+Age :  15
+Birthplace :  Bangkok
+Status :  Team BIII
+
+>  Jane
+FullName :  Kunjiranut Intarasin
+Age :  17
+Birthplace :  Nonthaburi
+Status :  Team BIII
+
+>  Jennis
+FullName :  Jennis Oprasert
+Age :  17
+Birthplace :  Phetchaburi
+Status :  Team BIII
+
+>  Jib
+FullName :  Suchaya Saenkhot
+Age :  15
+Birthplace :  Lop Buri
+Status :  Team BIII
+
+>  Kaew
+FullName :  Natruja Chutiwansopon
+Age :  23
+Birthplace :  Chon Buri
+Status :  Team BIII
+
+>  Kaimook
+FullName :  Warattaya Deesomlert
+Age :  20
+Birthplace :  Bangkok
+Status :  Team BIII
+
+>  Kate
+FullName :  Korapat Nilprapa
+Age :  16
+Birthplace :  Phayao
+Status :  Team BIII
+
+>  Korn
+FullName :  Vathusiri Phuwapunyasiri
+Age :  19
+Birthplace :  Bangkok
+Status :  Team BIII
+
+>  Mind
+FullName :  Panisa Srilaloeng
+Age :  16
+Birthplace :  Nakhon Ratchasima
+Status :  Team BIII
+
+>  Miori
+FullName :  Miori Ohkubo
+Age :  19
+Birthplace :  Ibaraki (Japan)
+Status :  Team BIII
+
+>  Mobile
+FullName :  Pimrapat Phadungwatanachok
+Age :  15
+Birthplace :  Bangkok
+Status :  Team BIII
+
+>  Music
+FullName :  Praewa Suthamphong
+Age :  16
+Birthplace :  Bangkok
+Status :  Team BIII
+
+>  Namnueng
+FullName :  Milin Dokthian
+Age :  21
+Birthplace :  Sing Buri
+Status :  Team BIII
+
+>  Namsai
+FullName :  Pichayapa Natha
+Age :  18
+Birthplace :  Bangkok
+Status :  Team BIII
+
+>  Noey
+FullName :  Kanteera Wadcharathadsanakul
+Age :  20
+Birthplace :  Samut Prakan
+Status :  Team BIII
+
+>  Orn
+FullName :  Patchanan Jiajirachote
+Age :  21
+Birthplace :  Bangkok
+Status :  Team BIII
+
+>  Pun
+FullName :  Punsikorn Tiyakorn
+Age :  17
+Birthplace :  Bangkok
+Status :  Team BIII
+
+>  Pupae
+FullName :  Jiradapa Intajak
+Age :  20
+Birthplace :  Chiang Rai
+Status :  Team BIII
+
+>  Satchan
+FullName :  Sachiya Hanami / Sawitchaya Kajonrungsilp
+Age :  14
+Birthplace :  Bangkok
+Status :  Team BIII
+
+>  Tarwann
+FullName :  Isarapa Thawatpakdee
+Age :  21
+Birthplace :  Nakhon Pathom
+Status :  Team BIII
+
+>  Maysa
+FullName :  Mesa Chinavicharana
+Age :  18
+Birthplace :  Bangkok
+Status :  Kenkyūsei
+
+>  Namhom
+FullName :  Christin Larsen
+Age :  15
+Birthplace :  Sisaket
+Status :  Kenkyūsei
+
+>  Nink
+FullName :  Mananya Kaoju
+Age :  18
+Birthplace :  Samut Sakhon
+Status :  Kenkyūsei
+
+>  Piam
+FullName :  Nayika Srinian
+Age :  14
+Birthplace :  Bangkok
+Status :  Kenkyūsei
+
+>  Cincin
+FullName :  Rinrada Inthaisong
+Age :  17
+Birthplace :  Bangkok
+Status :  Graduation
+
+>  Kitkat
+FullName :  Watsamon Phongwanit
+Age :  16
+Birthplace :  Bangkok
+Status :  Graduation
+
+>  Jan
+FullName :  Jetsupa Kruetang
+Age :  23
+Birthplace :  Singburi
+Status :  Graduation
+
+"""
